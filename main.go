d2cf7e593de77bc49a98a76fd1ee25f49bdaf795
Kenta Kudo <yd37574@gmail.com>
2019-11-17 18:02:44 +0000

    http server

--- main.go
+++ main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"fmt"
 	cli "github.com/jawher/mow.cli"
+	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
+	"net"
+	"net/http"
+	"strconv"
 
 	"os"
 )
@@ -17,8 +22,33 @@ const (
 func main() {
 	app := cli.App(appName, appDesc)
 
+	srvPort := app.Int(cli.IntOpt{
+		Name:   "srv-port",
+		Desc:   "http server port",
+		EnvVar: "SRV_PORT",
+		Value:  8080,
+	})
+
 	app.Action = func() {
 		log.WithField("git_hash", gitHash).Println("Hello, world")
+
+		errCh := make(chan error, 1)
+
+		go func() {
+			http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
+				fmt.Fprintln(w, "Hello, world")
+			})
+			if err := http.ListenAndServe(net.JoinHostPort("", strconv.Itoa(*srvPort)), nil); err != nil {
+				errCh <- errors.Wrap(err, "server")
+			}
+		}()
+
+		select {
+		case err := <-errCh:
+			log.Println(err)
+		}
+
+		log.Println("bye")
 	}
 
 	if err := app.Run(os.Args); err != nil {
