86bd5b5d84ec8b02329d80e62c5c76ecf2ded099
Kenta Kudo <yd37574@gmail.com>
2019-11-17 17:37:17 +0000

    Insert git hash

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/KentaKudo/qiita-advent-calendar-2019
 
 go 1.13
 
-require github.com/jawher/mow.cli v1.1.0
+require (
+	github.com/jawher/mow.cli v1.1.0
+	github.com/sirupsen/logrus v1.4.2
+)
--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	cli "github.com/jawher/mow.cli"
+	log "github.com/sirupsen/logrus"
 
-	"log"
 	"os"
 )
+
+var gitHash = "overriden at compile time"
 
 const (
 	appName = "qiita-advent-calendar-2019"
@@ -16,10 +18,10 @@ func main() {
 	app := cli.App(appName, appDesc)
 
 	app.Action = func() {
-		log.Println("Hello, world")
+		log.WithField("git_hash", gitHash).Println("Hello, world")
 	}
 
 	if err := app.Run(os.Args); err != nil {
-		log.Fatal(err)
+		log.WithError(err).Fatal("app run")
 	}
 }
