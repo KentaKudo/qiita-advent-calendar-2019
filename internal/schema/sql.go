395149f7219511f2d72a09cd935e53f56fd29255
KentaKudo <yd37574@gmail.com>
2019-11-26 19:05:52 +0000

    fix typo

--- internal/schema/sql.go
+++ internal/schema/sql.go
@@ -4,12 +4,12 @@ var schemas = map[int]string{
 	0: `
 BEGIN;
 
-CREATE TABLE schema_version {
+CREATE TABLE schema_version ( 
         id        SMALLSERIAL NOT NULL PRIMARY KEY,
 	md_insert BIGINT      DEFAULT  EXTRACT(EPOCH FROM current_timestamp)::INT,
 	md_update BIGINT      DEFAULT  0,
 	md_curr   BOOL        DEFAULT  'true'
-};
+);
 
 INSERT INTO schema_version VALUES (0);
 
