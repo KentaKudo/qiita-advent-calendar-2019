c51f7ee213efa7455c8bef3ee5fb4fd61b4a86e7
Kenta Kudo <yd37574@gmail.com>
2019-12-05 19:12:04 +0000

    initialiseKafkaSink

--- go.mod
+++ go.mod
@@ -3,15 +3,16 @@ module github.com/KentaKudo/qiita-advent-calendar-2019
 go 1.13
 
 require (
-	github.com/gogo/protobuf v1.1.1
-	github.com/golang/mock v1.1.1
+	github.com/Shopify/sarama v1.24.1
+	github.com/gogo/protobuf v1.2.1
+	github.com/golang/mock v1.2.0
 	github.com/google/uuid v1.1.1
 	github.com/jawher/mow.cli v1.1.0
 	github.com/lib/pq v1.2.0
 	github.com/pkg/errors v0.8.1
-	github.com/prometheus/client_golang v1.2.1 // indirect
 	github.com/sirupsen/logrus v1.4.2
-	github.com/stretchr/testify v1.3.0
+	github.com/stretchr/testify v1.4.0
 	github.com/utilitywarehouse/go-operational v0.0.0-20190722153447-b0f3f6284543
+	github.com/uw-labs/substrate v0.0.0-20191204104633-135e0c1735ca
 	google.golang.org/grpc v1.25.1
 )
--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"os"
 	"os/signal"
 	"strconv"
+	"strings"
 	"sync"
 	"syscall"
 
@@ -14,6 +15,8 @@ import (
 	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/utilitywarehouse/go-operational/op"
+	"github.com/uw-labs/substrate"
+	"github.com/uw-labs/substrate/kafka"
 	"google.golang.org/grpc"
 
 	_ "github.com/lib/pq"
@@ -58,6 +61,25 @@ func main() {
 		Value:  defaultSchemaVersion,
 	})
 
+	sinkKafkaVersion := app.String(cli.StringOpt{
+		Name:   "sink-kafka-version",
+		Desc:   "sink kafka version",
+		EnvVar: "SINK_KAFKA_VERSION",
+	})
+	sinkBrokers := app.String(cli.StringOpt{
+		Name:   "sink-brokers",
+		Desc:   "kafka sink brokers",
+		EnvVar: "SINK_BROKERS",
+		Value:  "localhost:9092",
+	})
+
+	actionTopic := app.String(cli.StringOpt{
+		Name:   "action-topic",
+		Desc:   "action topic",
+		EnvVar: "ACTION_TOPIC",
+		Value:  "qiita.action",
+	})
+
 	app.Action = func() {
 		log.WithField("git_hash", gitHash).Println("Hello, world")
 
@@ -79,6 +101,12 @@ func main() {
 		defer lis.Close()
 
 		gSrv := initialiseGRPCServer(newServer(store))
+
+		actionSink, err := initialiseKafkaSink(sinkKafkaVersion, sinkBrokers, actionTopic, actionKeyFunc)
+		if err != nil {
+			log.Fatalln("init payment account kafka sink:", err)
+		}
+		defer actionSink.Close()
 
 		errCh := make(chan error, 2)
 
@@ -136,4 +164,18 @@ func initialiseGRPCServer(srv service.TodoAPIServer) *grpc.Server {
 
 	service.RegisterTodoAPIServer(gSrv, srv)
 	return gSrv
+}
+
+func initialiseKafkaSink(version, brokers, topic *string, keyFunc func(substrate.Message) []byte) (substrate.SynchronousMessageSink, error) {
+	sink, err := kafka.NewAsyncMessageSink(kafka.AsyncMessageSinkConfig{
+		Brokers: strings.Split(*brokers, ","),
+		Topic:   *topic,
+		KeyFunc: keyFunc,
+		Version: *version,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return substrate.NewSynchronousMessageSink(sink), nil
 }
